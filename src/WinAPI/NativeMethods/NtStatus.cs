a7f6ec1fc7fa13cff68689d6965e54dee7de704a
Antoine Larine <antoine@larine.pro>
2024-01-25 12:39:00 +0300

    Add copyrights

--- src/WinAPI/NativeMethods/NtStatus.cs
+++ src/WinAPI/NativeMethods/NtStatus.cs
@@ -1,3 +1,6 @@
+// Copyright © Anton Larin, 2024. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace Larin.WinAPI.NativeMethods;
 
 /// <summary>
