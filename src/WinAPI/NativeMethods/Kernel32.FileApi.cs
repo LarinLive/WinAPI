86699261cb73739e0fc2578ad35c39a2bb345fad
AntonLarinLive <18666703+LarinLive@users.noreply.github.com>
2025-03-24 23:48:24 +0300

    Add IOCTL_DISK_GET_LENGTH_INFO.

--- src/WinAPI/NativeMethods/Kernel32.FileApi.cs
+++ src/WinAPI/NativeMethods/Kernel32.FileApi.cs
@@ -309,38 +309,6 @@ public static unsafe partial class Kernel32
 	#endregion
 
 	/// <summary>
-	/// Sends a control code directly to a specified device driver, causing the corresponding device to perform the corresponding operation.
-	/// </summary>
-	/// <param name="hDevice">A handle to the device on which the operation is to be performed. The device is typically a volume, directory, file, or stream. 
-	/// To retrieve a device handle, use the <see cref="CreateFile"/> function.</param>
-	/// <param name="dwIoControlCode">The control code for the operation. This value identifies the specific operation to be performed and the type of device on which to perform it.</param>
-	/// <param name="lpInBuffer">A pointer to the input buffer that contains the data required to perform the operation. The format of this data depends on the value of the dwIoControlCode parameter.
-	/// This parameter can be NULL if dwIoControlCode specifies an operation that does not require input data.</param>
-	/// <param name="nInBufferSize">The size of the input buffer, in bytes.</param>
-	/// <param name="lpOutBuffer">A pointer to the output buffer that is to receive the data returned by the operation. The format of this data depends on the value of the dwIoControlCode parameter.
-	/// This parameter can be NULL if dwIoControlCode specifies an operation that does not return data.</param>
-	/// <param name="nOutBufferSize">The size of the output buffer, in bytes.</param>
-	/// <param name="lpBytesReturned">A pointer to a variable that receives the size of the data stored in the output buffer, in bytes.</param>
-	/// <param name="lpOverlapped">A pointer to an <see cref="OVERLAPPED"/> structure. If hDevice was opened without specifying <see cref="FILE_FLAG_OVERLAPPED"/>, lpOverlapped is ignored.
-	/// If hDevice was opened with the <see cref="FILE_FLAG_OVERLAPPED"/> flag, the operation is performed as an overlapped(asynchronous) operation.
-	/// In this case, lpOverlapped must point to a valid <see cref="OVERLAPPED"/> structure that contains a handle to an event object. Otherwise, the function fails in unpredictable ways.
-	/// For overlapped operations, <see cref="DeviceIoControl"/> returns immediately, and the event object is signaled when the operation has been completed. 
-	/// Otherwise, the function does not return until the operation has been completed or an error occurs.</param>
-	/// <returns></returns>
-	/// <remarks>https://learn.microsoft.com/en-us/windows/win32/api/ioapiset/nf-ioapiset-deviceiocontrol</remarks>
-	[DllImport(Kernel32Lib, CharSet = CharSet.Unicode, SetLastError = true)]
-	public static extern bool DeviceIoControl(
-		[In] nint hDevice,
-		[In] uint dwIoControlCode,
-		[In, Optional] void* lpInBuffer,
-		[In] uint nInBufferSize,
-		[Out, Optional] void* lpOutBuffer,
-		[In] uint nOutBufferSize,
-		[Out, Optional] uint* lpBytesReturned,
-		[In, Out, Optional] OVERLAPPED* lpOverlapped
-	);
-
-	/// <summary>
 	/// Retrieves the name of a volume on a computer. <see cref="FindFirstVolumeW"/> is used to begin scanning the volumes of a computer.
 	/// </summary>
 	/// <param name="lpszVolumeName">A pointer to a buffer that receives a null-terminated string that specifies a volume GUID path for the first volume that is found.</param>
