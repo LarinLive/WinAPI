2cbc9c5600bb5536c769375afdfed4004a833610
AntonLarinLive <18666703+LarinLive@users.noreply.github.com>
2025-03-25 13:47:50 +0300

    Stub.

--- src/WinAPI/NativeMethods/SetupApi2.cs
+++ src/WinAPI/NativeMethods/SetupApi2.cs
@@ -9,7 +9,7 @@ namespace LarinLive.WinAPI.NativeMethods;
 /// <summary>
 /// P/Invoke items for the SetupAPI.dll Windows API library
 /// </summary>
-public static unsafe class SetupAPI
+public static unsafe class SetupApi2
 {
 	/// <summary>
 	/// SetupAPI library file name
